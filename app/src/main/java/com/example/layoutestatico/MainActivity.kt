2566e47ce3acd05d2a63e3f8903390b014a0f845
DANIELE SILVA SANTOS <25203622@sn1271249388.local>
2026-09-15 10:38:00 -0300

    feat: adiciona o botão PRESS START e centraliza ele no final da tela

--- app/src/main/java/com/example/layoutestatico/MainActivity.kt
+++ app/src/main/java/com/example/layoutestatico/MainActivity.kt
@@ -6,12 +6,19 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -20,9 +27,9 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.layoutestatico.ui.theme.LayoutEstaticoTheme
 
 class MainActivity : ComponentActivity() {
@@ -44,54 +51,65 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun BasicComponentsScreen(modifier: Modifier = Modifier) {
 
-    Column(modifier = modifier
+    // coluna base da tela
+    Column(
+        modifier = modifier
         .fillMaxSize()
-        .background(Color(0, 0, 0, 255))
+        .background(Color.Black),
+        verticalArrangement = Arrangement.SpaceBetween
     ){
-        Row(verticalAlignment = Alignment.CenterVertically){
+        Row(
+            verticalAlignment = Alignment.CenterVertically
+        ){
             Text(
                 text = "Score: 0050",
                 fontWeight = FontWeight.Bold,
-                color = Color(0xFFFFFFFF),
-                fontFamily = FontFamily.Serif
+                color = Color.White
             )
 
+            // empurra o Score totalmente para a esquerda e o
+            // painel de vidas totalmente para a direita
             Spacer(
                 modifier = Modifier.weight(1f)
             )
 
+            // informações do painel de vidas
             Text(
                 text = "Lives: ",
                 fontWeight = FontWeight.Bold,
-                color = Color(0xFFFFFFFF),
-                fontFamily = FontFamily.Serif,
+                color = Color.White,
                 modifier = Modifier
             )
 
             AndroidEnemy(
-                modifier = Modifier.size(30.dp)
+                modifier = Modifier
+                    .size(30.dp)
                     .padding(2.dp),
                 color = Color.Green
             )
 
             AndroidEnemy(
-                modifier = Modifier.size(30.dp)
+                modifier = Modifier
+                    .size(30.dp)
                     .padding(2.dp),
                 color = Color.Green
             )
 
             AndroidEnemy(
-                modifier = Modifier.size(30.dp)
+                modifier = Modifier
+                    .size(30.dp)
                     .padding(2.dp),
                 color = Color.Green
             )
 
         }
+
+        // fileira dos inimigos (verde, vermelho, azul, amarelo, verde)
         Column {
             Row {
                 AndroidEnemy(
                     modifier = Modifier.size(80.dp),
-                    color = Color(124, 248, 110, 255)
+                    color = Color.Green
                 )
 
                 AndroidEnemy(
@@ -114,6 +132,43 @@ fun BasicComponentsScreen(modifier: Modifier = Modifier) {
                     color = Color.Green
                 )
             }
+        }
+
+        Box(
+            modifier = Modifier
+                .padding(bottom = 32.dp)
+                .size(60.dp),
+            contentAlignment = Alignment.Center
+        ) {
+            // Image(painter = painterResource(R.drawable.nome_do_seu_cubo),
+            // contentDescription = null)
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(Color(0xFF4285F4), shape = RoundedCornerShape(12.dp))
+            )
+            Box(
+                modifier = Modifier
+                    .size(40.dp)
+                    .background(Color(0xFF34A853), shape = RoundedCornerShape(8.dp))
+            )
+        }
+
+        // Botão de start
+        Button (
+            onClick = {},
+            colors = ButtonDefaults.buttonColors(containerColor = Color(0xFF2D2D2D)),
+            shape = RoundedCornerShape(8.dp),
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(55.dp)
+        ) {
+            Text(
+                text = "PRESS START",
+                color = Color.White,
+                fontSize = 18.sp,
+                fontWeight = FontWeight.Bold,
+            )
         }
     }
 }
